f56bfa9223df7b3aee88966266902cfb18667681
Jonathan Zhang <78021210+Zzzz-Jonathan@users.noreply.github.com>
2022-04-20 16:12:30 +0800

    4/20

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import Workbook
 
-
-# //*[@id="highcharts-0"]
 
 class DataCollector:
     def __init__(self, urls):
@@ -15,6 +14,8 @@ class DataCollector:
         self.chart_path = ['//*[@id="__next"]/div/div[2]/div[1]/div[3]/div[2]/div[2]/div[2]/div/div/iframe',
                            '//*[@id="sub-container"]/div/div/div[1]/div/div/div[1]/div/div/div/div[2]/div/div[3]']
         # self.chart_path = '/html/body/div[1]/nav/div/ul[1]/li[8]/a'
+        self.current_data = []
+        self.name = ''
 
         options = Options()
         options.add_argument("--disable-blink-features")
@@ -24,6 +25,11 @@ class DataCollector:
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 10)
         self.action = ActionChains(self.driver)
+
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.row = 1
+        self.col = 1
 
     def login(self, url, name, password):
         self.driver.get(url)
@@ -49,14 +55,20 @@ class DataCollector:
 
         loginbtn.click()
 
-    def switch(self):
+    def switch(self, url=None):
+        if url is not None:
+            self.driver.get(url)
+
+        time.sleep(5)  # wait website loading
+
         windows = self.driver.window_handles
         self.driver.switch_to.window(windows[-1])
 
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, self.chart_path[0]))
 
     def get_inf(self):
-        print(self.driver.title)
+        self.name = self.driver.title
+        print(self.name + ' start')
         try:
             charts = self.wait.until(EC.presence_of_element_located((By.XPATH, self.chart_path[1])))
             # charts = self.driver.find_element(By.XPATH, self.chart_path)
@@ -69,16 +81,52 @@ class DataCollector:
             self.action.move_to_element_with_offset(charts, x, 200).perform()
 
             try:
-                thead = self.driver.find_element(By.XPATH, '//*[@id="highcharts-0"]/div/span/div/table/thead/tr/td/span')
+                thead = self.driver.find_element(By.XPATH,
+                                                 '//*[@id="highcharts-0"]/div/span/div/table/thead/tr/td/span')
                 tbody = self.driver.find_element(By.XPATH, '//*[@id="highcharts-0"]/div/span/div/table/tbody')
             except:
                 print("Haven't find yet")
             else:
-                print(thead, tbody)
+                time_inf = thead.text
+                legends = tbody.find_elements(By.CLASS_NAME, 'legend')
+                values = tbody.find_elements(By.CLASS_NAME, 'value')
+                print(legends[0].text)
+
+                legend_infs = [legend.find_element(By.CLASS_NAME, 'label').text for legend in legends]
+                value_infs = [value.find_element(By.TAG_NAME, 'b').text for value in values]
+
+                if time_inf != self.current_data[len(self.current_data) - 1][0]:
+                    collect = [time_inf, legend_infs, legend_infs, value_infs]
+                    self.current_data.append(collect)
+
             finally:
                 pass
 
             time.sleep(0.02)
+
+    def toxlsx(self):
+        length = len(self.current_data[0][1]) + 1
+        col = self.col
+        for c in range(col, col + length):
+            self.ws.cell(self.row, c).value = self.name
+        self.row += 1
+        label = 0
+        for c in range(col + 1, col + length):
+            self.ws.cell(self.row, c).value = self.current_data[0][1][label]
+            label += 1
+        self.row += 1
+
+        for data in self.current_data:
+            self.ws.cell(self.row, col).value = data[0]
+            for value, c in zip(data[2], range(col + 1, col + length)):
+                self.ws.cell(self.row, c).value = value
+            self.row += 1
+
+        self.row = 1
+        self.col = col + length
+
+        self.current_data.clear()
+        print(self.name + ' finish')
 
 
 if __name__ == '__main__':
@@ -86,7 +134,8 @@ if __name__ == '__main__':
     account = 'dengqiaoyun@studio33.wecom.work'
     password = 'NC19980221dqy!'
     links = [
-        'http://www.minecraftxz.com/',
+        'https://www.data.ai/apps/ios/app/989673964/rank-history?app_slug=989673964&market_slug=ios&vtype=day'
+        '&countries=CN&device=iphone&view=rank&legends=2222&date=2022-02-01~2022-04-18',
         'https://www.data.ai/apps/ios/app/654897098/rank-history?vtype=day&countries=CN&device=iphone&view=grossing'
         '&legends=2222&date=2022-02-01~2022-04-18&market_slug=ios&app_slug=654897098 '
     ]
@@ -94,7 +143,9 @@ if __name__ == '__main__':
     dc = DataCollector(links)
     dc.login(dc.urls[1], account, password)  # login data.ai with given user
     time.sleep(5)  # wait website loading
-    dc.switch()
-    print('start getting')
-    dc.get_inf()  # moving the mouse and collect inf
-    print('end')
+    for app in dc.urls:
+        dc.switch(app)
+        dc.get_inf()  # moving the mouse and collect inf
+        dc.toxlsx()  # push data into xlsx
+
+    dc.wb.save('rank_data.xlsx')
